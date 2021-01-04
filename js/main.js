aa8d4b972978b12927783f0b5435913e8fdabc57
Emmy <60683173+emmy-html@users.noreply.github.com>
2021-01-04 14:19:08 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function menu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display !== 'block') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
