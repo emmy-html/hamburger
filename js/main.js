230bf871879a9f816330188f64e5c71e3b6a302e
Emmy <60683173+emmy-html@users.noreply.github.com>
2021-01-04 17:41:59 -0600

    Refactored code into one if/else statement so it checks for menu visibility & changes hamburger icon at the same time

--- js/main.js
+++ js/main.js
@@ -3,14 +3,10 @@ function menu() {
     var hamburger = document.getElementById("hamburger");
     if (menu.style.display !== 'block') {
         menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-    // do the same thing but for changing the hamburger class name??
-    if (hamburger.className == 'fas', 'fa-hamburger' && menu.style.display == 'block') {
         hamburger.classList.remove('fa-hamburger');
         hamburger.classList.add('fas', 'fa-times-circle', 'fa-2x', 'menu-open');
     } else {
+        menu.style.display = 'none';
         hamburger.classList.remove('fa-times-circle');
         hamburger.classList.add('fa-hamburger');
     }
