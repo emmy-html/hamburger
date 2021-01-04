2690c1d72e45066e411d780924929355de801978
Emmy <60683173+emmy-html@users.noreply.github.com>
2021-01-04 17:28:42 -0600

    Added mechanics to change the icon to a close symbol on click, and then back to hamburger when menu is closed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 function menu() {
     var menu = document.getElementById("menu");
+    var hamburger = document.getElementById("hamburger");
     if (menu.style.display !== 'block') {
         menu.style.display = 'block';
     } else {
         menu.style.display = 'none';
+    }
+    // do the same thing but for changing the hamburger class name??
+    if (hamburger.className == 'fas', 'fa-hamburger' && menu.style.display == 'block') {
+        hamburger.classList.remove('fa-hamburger');
+        hamburger.classList.add('fas', 'fa-times-circle', 'fa-2x', 'menu-open');
+    } else {
+        hamburger.classList.remove('fa-times-circle');
+        hamburger.classList.add('fa-hamburger');
     }
 }
