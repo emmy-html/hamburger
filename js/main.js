7b47a2dc3c627698a575b775800073a1c6877d27
Emmy <60683173+emmy-html@users.noreply.github.com>
2021-04-19 15:34:25 -0500

    Added submenu feature that hides and displays sub-menus on hover

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ function menu() {
         hamburger.classList.add('fa-hamburger');
     }
 }
+function submenu(num) {
+    const submenuAnchor = document.querySelectorAll(".sub-menu-anchor");
+    const theSubMenus = document.querySelectorAll(".sub-menu");
+        if (theSubMenus[num].style.display != 'block') {
+            theSubMenus[num].style.display = "block";
+        }
+        else {
+            theSubMenus[num].style.display = 'none';
+        }
+    submenuAnchor[num].onmouseleave = function reset() {
+        theSubMenus[num].style.display = "none";
+    }
+}
